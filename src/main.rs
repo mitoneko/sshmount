ac3af9860ff80116ffb000101f64a8556c8811df
mitoneko <facemito@laki.jp>
2022-12-24 15:40:30 +0900

    コマンドライン解析:mount先の存在チェック

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
 use clap::Parser;
 
+fn main() {
+    let opt = Opt::parse();
+
+    println!("\"{}\"をマウントする予定", opt.remote);
+    println!("\"{}\"にマウントされる予定", opt.mount_point);
+}
+
+/// コマンドラインオプション
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Opt {
     /// 接続先のアドレスまたはDNS名
     remote: String,
     /// マウント先のパス
+    #[arg(value_parser = exist_dir)]
     mount_point: String,
 }
 
-fn main() {
-    let opt = Opt::parse();
+/// 指定されたディレクトリが存在し、中にファイルがないことを確認する。
+fn exist_dir(s: &str) -> Result<String, String> {
+    match std::fs::read_dir(s) {
+        Ok(mut dir) => match dir.next() {
+            None => Ok(s.to_string()),
+            Some(_) => Err("マウント先ディレクトリが空ではありません".to_string()),
+        },
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::NotFound => {
+                Err("マウント先ディレクトリが存在しません。".to_string())
+            }
+            _ => Err("計り知れないエラーです。".to_string()),
+        },
+    }
+}
 
-    println!("{}", opt.remote);
-    println!("{}", opt.mount_point);
+#[test]
+fn verify_cli() {
+    use clap::CommandFactory;
+    Opt::command().debug_assert()
 }
