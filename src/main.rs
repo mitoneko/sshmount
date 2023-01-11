03216ac3ed1a753acd641f63275bc9908fd3d3b1
mitoneko <facemito@laki.jp>
2023-01-11 22:14:53 +0900

    コマンドライン解析　リモートホスト指定部の実装
    user,host,pathを認識完了。(RemoteName新設。及び、FromStrの実装。)
    アドレスのlookup対象をremote.hostに変更。
    FromStrのテストを追加。

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,10 @@ use std::net::TcpStream;
 fn main() {
     let opt = Opt::parse();
     env_logger::init();
-    let options = vec![fuser::MountOption::FSName("sshfs".to_string())];
 
     println!("\"{}\"をマウントする予定", opt.remote);
-    // 今は、固定接続先に、固定接続
-    let address = lookup_host("reterminal.local").unwrap();
+    // ホスト名の最終決定には、.ssh/config の情報反映が必要
+    let address = lookup_host(&opt.remote.host).unwrap();
     if address.is_empty() {
         panic!("not found");
     }
@@ -23,10 +22,12 @@ fn main() {
     let mut ssh = Session::new().unwrap();
     ssh.set_tcp_stream(tcp);
     ssh.handshake().unwrap();
+    // 認証情報は、今は固定
     let key = std::path::Path::new("/home/mito/.ssh/id_rsa");
     ssh.userauth_pubkey_file("mito", None, key, None).unwrap();
     let fs = ssh_filesystem::Sshfs::new(ssh);
 
+    let options = vec![fuser::MountOption::FSName("sshfs".to_string())];
     fuser::mount2(fs, opt.mount_point, &options).unwrap();
 }
 
@@ -34,8 +35,8 @@ fn main() {
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Opt {
-    /// 接続先のアドレスまたはDNS名
-    remote: String,
+    /// 接続先 [user@]host:[path]
+    remote: RemoteName,
     /// マウント先のパス
     #[arg(value_parser = exist_dir)]
     mount_point: String,
@@ -57,8 +58,114 @@ fn exist_dir(s: &str) -> Result<String, String> {
     }
 }
 
-#[test]
-fn verify_cli() {
-    use clap::CommandFactory;
-    Opt::command().debug_assert()
+/// コマンドラインの接続先ホスト情報
+#[derive(Clone, Debug, PartialEq)]
+struct RemoteName {
+    /// ユーザー名
+    user: Option<String>,
+    /// ホスト名　または　IPアドレス
+    host: String,
+    /// 接続先パス
+    path: Option<std::path::PathBuf>,
+}
+
+impl std::fmt::Display for RemoteName {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = format!("<{:?}><{:?}><{:?}>", &self.user, &self.host, &self.path);
+        s.fmt(f)
+    }
+}
+
+impl std::str::FromStr for RemoteName {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut rest_str = s;
+        let user = match rest_str.split_once('@') {
+            Some((u, r)) => {
+                rest_str = r;
+                if !u.trim().is_empty() {
+                    Some(u.trim().to_string())
+                } else {
+                    None
+                }
+            }
+            None => None,
+        };
+        let (host, path) = match rest_str.split_once(':') {
+            Some((h, p)) => (
+                h.trim().to_string(),
+                if !p.trim().is_empty() {
+                    Some(std::path::PathBuf::from(p.trim().to_string()))
+                } else {
+                    None
+                },
+            ),
+            None => return Err("接続先ホストの形式は、\"[user@]host:[path]\"です。".to_string()),
+        };
+        Ok(Self { user, host, path })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn verify_cli() {
+        use clap::CommandFactory;
+        Opt::command().debug_assert()
+    }
+
+    #[test]
+    fn test_from_str_remotename() {
+        use std::path::Path;
+        let s = "mito@reterminal.local:/home/mito";
+        let r: RemoteName = s.parse().unwrap();
+        let k = RemoteName {
+            user: Some("mito".to_string()),
+            host: "reterminal.local".to_string(),
+            path: Some(Path::new("/home/mito").into()),
+        };
+        assert_eq!(r, k);
+
+        let s = "mito@reterminal.local:/home/mito/";
+        let r: RemoteName = s.parse().unwrap();
+        let k = RemoteName {
+            user: Some("mito".to_string()),
+            host: "reterminal.local".to_string(),
+            path: Some(Path::new("/home/mito").into()),
+        };
+        assert_eq!(r, k);
+
+        let s = "reterminal.local:";
+        let r: RemoteName = s.parse().unwrap();
+        let k = RemoteName {
+            user: None,
+            host: "reterminal.local".to_string(),
+            path: None,
+        };
+        assert_eq!(r, k);
+
+        let s = " mito @reterminal.local: ";
+        let r: RemoteName = s.parse().unwrap();
+        let k = RemoteName {
+            user: Some("mito".to_string()),
+            host: "reterminal.local".to_string(),
+            path: None,
+        };
+        assert_eq!(r, k);
+
+        let s = "reterminal.local";
+        let r: Result<RemoteName, String> = s.parse();
+        assert_eq!(
+            r,
+            Err("接続先ホストの形式は、\"[user@]host:[path]\"です。".to_string())
+        );
+
+        let s = "mito@reterminal.local";
+        let r: Result<RemoteName, String> = s.parse();
+        assert_eq!(
+            r,
+            Err("接続先ホストの形式は、\"[user@]host:[path]\"です。".to_string())
+        );
+    }
 }
